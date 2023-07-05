bfc73f5f7d09002e7741c9ffaf6b6325ada9c000
Siddharth Dholu <siddharthdholu@gmail.com>
2023-07-05 12:29:40 +0100

    First Commit!

---
+++ app/src/main/java/com/smd/agecalculatorandreminder/ui/screen/Login.kt
@@ -0,0 +1,194 @@
+package com.smd.agecalculatorandreminder.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF181A1F))
+            .padding(16.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Welcome Back!",
+                    color = Color(0xFFFFFFFF),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = "Please sign in to your account",
+                    color = Color(0xFF696A6F),
+                    fontSize = 15.sp
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Box(
+                    contentAlignment = Alignment.CenterStart,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .clip(RoundedCornerShape(6.dp))
+                        .background(Color(0xFF262A34))
+                        .padding(start = 10.dp)
+
+                ) {
+                    TextField(
+                        value = username, onValueChange = { username = it }, textStyle = TextStyle(
+                            fontSize = 16.sp, color = Color(0xFFFFFFFF),
+                        ), singleLine = true, maxLines = 1, placeholder = {
+                            Text(
+                                text = "Username", color = Color(0xFF696A6F), fontSize = 16.sp
+                            )
+                        }, colors = TextFieldDefaults.textFieldColors(
+                            containerColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                        ), modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Box(
+                    contentAlignment = Alignment.CenterStart,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .clip(RoundedCornerShape(6.dp))
+                        .background(Color(0xFF262A34))
+                        .padding(start = 10.dp)
+
+                ) {
+                    TextField(
+                        value = password, onValueChange = { password = it }, textStyle = TextStyle(
+                            fontSize = 16.sp, color = Color(0xFFFFFFFF),
+                        ), singleLine = true, maxLines = 1, placeholder = {
+                            Text(
+                                text = "Password", color = Color(0xFF696A6F), fontSize = 16.sp
+                            )
+                        }, colors = TextFieldDefaults.textFieldColors(
+                            containerColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                        ), modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Forgot Password?", fontSize = 14.sp, color = Color(0xFF696A6F)
+                    )
+                }
+                Spacer(modifier = Modifier.height(35.dp))
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .clip(RoundedCornerShape(6.dp))
+                        .background(Color(0xFF5467FF))
+                        .padding(start = 10.dp)
+                ) {
+                    Text(
+                        text = "Sign In",
+                        color = Color(0xFFFFFFFF),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .clip(RoundedCornerShape(6.dp))
+                        .background(Color(0xFFFFFFFF))
+                        .padding(start = 10.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Face,
+                        contentDescription = "Google Button",
+                        tint = Color.Black
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Sign in with Google",
+                        color = Color(0xFF181A1F),
+                        fontWeight = FontWeight(600)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen()
+}
